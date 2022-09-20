160d7fc8b910db81cb254d0a24bc2371bf4a136c
Carlo Zottmann <carlo@zottmann.org>
2022-09-20 19:11:41 +0200

    [NEW] Adds files

---
+++ bin/release.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Thanks to https://github.com/obsidian-tasks-group/obsidian-tasks for this
+# script!
+
+set -euo pipefail
+
+if [ "$#" -ne 2 ]; then
+    echo "Must provide exactly two arguments."
+    echo "First one must be the new version number."
+    echo "Second one must be the minimum obsidian version for this release."
+    echo ""
+    echo "Example usage:"
+    echo "./release.sh 0.3.0 0.11.13"
+    echo "Exiting."
+
+    exit 1
+fi
+
+if [[ $(git status --porcelain) ]]; then
+  echo "Changes in the git repo."
+  echo "Exiting."
+
+  exit 1
+fi
+
+NEW_VERSION=$1
+MINIMUM_OBSIDIAN_VERSION=$2
+
+echo "Updating to version ${NEW_VERSION} with minimum Obsidian version ${MINIMUM_OBSIDIAN_VERSION}"
+
+read -p "Continue? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Updating package.json"
+  TEMP_FILE=$(mktemp)
+  jq ".version |= \"${NEW_VERSION}\"" package.json > "$TEMP_FILE" || exit 1
+  mv "$TEMP_FILE" package.json
+
+  echo "Updating manifest.json"
+  TEMP_FILE=$(mktemp)
+  jq ".version |= \"${NEW_VERSION}\" | .minAppVersion |= \"${MINIMUM_OBSIDIAN_VERSION}\"" manifest.json > "$TEMP_FILE" || exit 1
+  mv "$TEMP_FILE" manifest.json
+
+  echo "Updating versions.json"
+  TEMP_FILE=$(mktemp)
+  jq ". += {\"${NEW_VERSION}\": \"${MINIMUM_OBSIDIAN_VERSION}\"}" versions.json > "$TEMP_FILE" || exit 1
+  mv "$TEMP_FILE" versions.json
+
+  read -p "Create git commit, tag, and push? [y/N] " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    git add -A .
+    git commit -m"[REL] Releases version ${NEW_VERSION}"
+    git tag "${NEW_VERSION}"
+    git push
+    LEFTHOOK=0 git push --tags
+  fi
+
+else
+  echo "Exiting."
+  exit 1
+fi
