467d2a51f74c9598cad5a83fc3f261e892859ec5
Carlo Zottmann <carlo@zottmann.org>
2022-10-01 17:37:21 +0200

    [REL] Releases version 0.10.2

--- src/plugin-info.ts
+++ src/plugin-info.ts
@@ -1,4 +1,4 @@
 /* File will be overwritten by bin/release.sh! */export const PLUGIN_INFO = {
-  "pluginVersion": "0.10.1",
-  "pluginReleasedAt": "2022-10-01T13:20:30+0200"
+  "pluginVersion": "0.10.2",
+  "pluginReleasedAt": "2022-10-01T17:37:16+0200"
 }
