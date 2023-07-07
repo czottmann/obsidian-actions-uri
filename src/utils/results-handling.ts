b4e0d9d8bbd332594e3087afc176f7be8f76fa3b
Carlo Zottmann <carlo@zottmann.org>
2023-07-07 10:32:28 +0200

    [NEW] Adds docs

--- src/utils/results-handling.ts
+++ src/utils/results-handling.ts
@@ -1,16 +1,30 @@
 import { ErrorObject, ResultObject } from "../types";
 
+/**
+ * Returns a `ResultObject` based on the passed-in parameters.
+ *
+ * @param result The `ResultObject`'s `result` key
+ * @param processedFilepath Optional, the `ResultObject`'s `processedFilepath` key
+ * @returns A `ResultObject` with the `isSuccess` key set to `true`
+ */
 export function success<T>(
-  arg: T,
+  result: T,
   processedFilepath?: string,
 ): ResultObject<T> {
   return {
     isSuccess: true,
-    result: arg,
+    result,
     processedFilepath,
   };
 }
 
+/**
+ * Returns an `ErrorObject` based on the passed-in parameters.
+ *
+ * @param errorCode The `ErrorObject`'s `errorCode` key
+ * @param errorMessage The `ErrorObject`'s `errorMessage` key
+ * @returns An `ErrorObject` with the `isSuccess` key set to `false`
+ */
 export function failure(errorCode: number, errorMessage: string): ErrorObject {
   return {
     isSuccess: false,
