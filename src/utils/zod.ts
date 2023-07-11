65f63227d8aef0e12c18d21fc9c5c5ef8824f8b5
Carlo Zottmann <carlo@zottmann.org>
2023-07-11 12:47:54 +0200

    [NEW] Adds zod schemas for checking for existing file paths

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { normalizePath, TFile, TFolder } from "obsidian";
 import { sanitizeFilePath } from "./file-handling";
 
 // The absence of a parameter `blah`, a `blah=false` and a value-less `blah=`
@@ -17,9 +18,21 @@ export const zodSanitizedFilePath = z.string()
   .min(1, { message: "can't be empty" })
   .transform((file) => sanitizeFilePath(file));
 
+export const zodExistingFilePath = z.preprocess(
+  (file) =>
+    window.app.vault.getAbstractFileByPath(normalizePath(file as string)),
+  z.instanceof(TFile, { message: "File doesn't exist" }),
+);
+
 export const zodSanitizedFolderPath = z.string()
   .min(1, { message: "can't be empty" })
   .transform((file) => sanitizeFilePath(file, true));
+
+export const zodExistingFolderPath = z.preprocess(
+  (folder) =>
+    window.app.vault.getAbstractFileByPath(normalizePath(folder as string)),
+  z.instanceof(TFolder, { message: "Folder doesn't exist" }),
+);
 
 /**
  * An always-false boolean. Looks stupid but it's used by the handlers in
