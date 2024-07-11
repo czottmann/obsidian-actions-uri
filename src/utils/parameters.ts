6de654ce2ab2c11d3837e4ca3765a4753bbab106
Carlo Zottmann <carlo@zottmann.dev>
2024-07-11 19:33:18 +0200

    [FIX] Adds lookup by UID in Markdown files in addition to metadata cache

--- src/utils/parameters.ts
+++ src/utils/parameters.ts
@@ -1,12 +1,12 @@
+import { parseFrontMatterEntry } from "obsidian";
 import { z } from "zod";
-import {
-  getPeriodNotePathIfPluginIsAvailable,
-  PeriodicNoteType,
-} from "./periodic-notes-handling";
-import { obsEnv } from "./obsidian-env";
-import { zodExistingNotePath } from "./zod";
 import { STRINGS } from "../constants";
+import { obsEnv } from "./obsidian-env";
+import { getPeriodNotePathIfPluginIsAvailable } from "./periodic-notes-handling";
+import { failure, success } from "./results-handling";
 import { NoteTargetingComputedValues, NoteTargetingParams } from "../schemata";
+import { StringResultObject } from "../types";
+import { zodExistingNotePath } from "./zod";
 
 /**
  * Validates the targeting parameters of a note and adds computed values.
@@ -104,19 +104,16 @@ function validateNoteTargetingAndResolvePath<T>(
     path = input.file;
   } else if (input.uid) {
     inputKey = "uid";
-    path = filepathForUID(input.uid);
+    const res = filepathForUID(input.uid);
+    path = res.isSuccess ? res.result : "";
   } else if (input["periodic-note"]) {
     inputKey = "periodic-note";
-    path = filepathForPeriodicNote(input["periodic-note"]);
+    const res = getPeriodNotePathIfPluginIsAvailable(input["periodic-note"]);
+    path = res.isSuccess ? res.result : "";
   }
 
-  const _computed = {
-    inputKey,
-    path,
-    pathExists: zodExistingNotePath.safeParse(path).success,
-  };
-
-  if (throwOnMissingNote && !_computed.pathExists) {
+  const pathExists = path != "" && zodExistingNotePath.safeParse(path).success;
+  if (!pathExists && throwOnMissingNote) {
     ctx.addIssue({
       code: z.ZodIssueCode.custom,
       message: STRINGS.note_not_found,
@@ -125,14 +122,31 @@ function validateNoteTargetingAndResolvePath<T>(
   }
 
   // Return original object plus computed values
-  return { ...data, _computed };
+  return {
+    ...data,
+    _computed: {
+      inputKey,
+      path,
+      pathExists,
+    },
+  };
 }
 
-function filepathForUID(uid: string): string {
+function filepathForUID(uid: string): StringResultObject {
+  const path = findPathForUIDInMetadataCache(uid) ||
+    findPathForUIDInMarkdownFiles(uid);
+
+  return path ? success(path) : failure(404, STRINGS.note_not_found);
+}
+
+function findPathForUIDInMetadataCache(uid: string): string | undefined {
+  // TODO: Make frontmatter key configurable
+  const uidKey = "uid";
+
   const hash = Object.entries(obsEnv.app.metadataCache.metadataCache)
     .find(([_, cached]) =>
-      cached.frontmatter?.uid &&
-      [cached.frontmatter.uid].flat().map((u) => `${u}`).includes(uid)
+      cached.frontmatter?.[uidKey] &&
+      [cached.frontmatter[uidKey]].flat().map((u) => `${u}`).includes(uid)
     )
     ?.[0];
 
@@ -140,10 +154,21 @@ function filepathForUID(uid: string): string {
     .find(([_, cache]) => cache.hash === hash)
     ?.[0];
 
-  return filePath || "";
+  return filePath;
 }
 
-function filepathForPeriodicNote(periodicID: PeriodicNoteType): string {
-  const res = getPeriodNotePathIfPluginIsAvailable(periodicID);
-  return res.isSuccess ? res.result : "";
+function findPathForUIDInMarkdownFiles(uid: string): string | undefined {
+  // TODO: Make frontmatter key configurable
+  const uidKey = "uid";
+
+  return obsEnv.app.vault
+    .getMarkdownFiles()
+    .find((note) => {
+      let uidValues = parseFrontMatterEntry(
+        obsEnv.app.metadataCache.getFileCache(note)?.frontmatter,
+        uidKey,
+      );
+      return [uidValues].flatMap((u) => `${u}`).includes(uid);
+    })
+    ?.path;
 }
