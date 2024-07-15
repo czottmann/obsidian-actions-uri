a34e03ab54b623fa779d76439f8a480ae32a715d
Carlo Zottmann <carlo@zottmann.dev>
2024-07-15 12:53:49 +0200

    [NEW] Sets plugin dev version

--- src/plugin-info.ts
+++ src/plugin-info.ts
@@ -1,5 +1,5 @@
 /* File will be overwritten by bin/release.sh! */
 export const PLUGIN_INFO = {
-  "pluginVersion": "1.5.3",
-  "pluginReleasedAt": "2024-06-18T15:07:53+0200"
-}
+  "pluginVersion": "1.6.x",
+  "pluginReleasedAt": "2024-06-18T15:07:53+0200",
+};
