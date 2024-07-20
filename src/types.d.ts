3391562f66645976d234599d8ddb69ae3aa49292
Carlo Zottmann <carlo@zottmann.dev>
2024-07-20 12:49:21 +0200

    [NEW] Adds `Prettify<…>` helper type

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,3 +6,13 @@ export * from "src/types/results";
 export type PluginSettings = {
   frontmatterKey: string;
 };
+
+/**
+ * A TypeScript type alias called `Prettify`.
+ * It takes a type as its argument and returns a new type that has the same properties as the original type,
+ * but the properties are not intersected. This means that the new type is easier to read and understand.
+ */
+export type Prettify<T> =
+  & { [K in keyof T]: T[K] extends object ? Prettify<T[K]> : T[K] }
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+  & unknown;
