272800b8a3cb1f0af5661d3a1376553d1ddf7b99
Carlo Zottmann <carlo@zottmann.dev>
2025-02-10 10:11:42 +0100

    [REL] Release 1.7.2

--- src/plugin-info.ts
+++ src/plugin-info.ts
@@ -1,5 +1,5 @@
 /* File will be overwritten by bin/release.sh! */
 export const PLUGIN_INFO = {
-  "pluginVersion": "post-create-pause.1",
-  "pluginReleasedAt": "2025-02-05T13:27:22+0100"
+  "pluginVersion": "1.7.2",
+  "pluginReleasedAt": "2025-02-10T10:11:37+0100"
 }
