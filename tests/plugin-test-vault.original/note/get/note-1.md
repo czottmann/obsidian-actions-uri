13348664f6ec3153039fe7964f6dc1ddfc65c7aa
Carlo Zottmann <carlo@zottmann.dev>
2025-05-18 13:10:33 +0200

    [NEW] Adds first real tests

---
+++ tests/plugin-test-vault.original/note/get/note-1.md
@@ -0,0 +1,8 @@
+---
+tags:
+  - one
+  - two
+id: 01JV9K2XGJA4HH5XVWKC8EPQ4W
+---
+
+# Hello
--- tests/plugin-test-vault.original/note/get/noteGet.test.ts
+++ tests/plugin-test-vault.original/note/get/noteGet.test.ts
@@ -1,18 +1,22 @@
 import { callObsidian } from "#tests/helpers";
 
-describe("Note Get Route", () => {
+describe("/note/get", () => {
   test("should return note content on success callback", async () => {
-    console.log(__dirname);
+    // console.log(__dirname);
 
-    const res = await callObsidian("note/get", { file: "Welcome.md" });
-
-    // TODO: Add specific assertions for note/get route
+    const res = await callObsidian("note/get", { file: "note/get/note-1.md" });
     expect(res.ok).toBe(true);
+
     if (res.ok) {
-      expect(res.value).toHaveProperty("result-content");
-      expect(res.value["result-content"]).toContain(
-        "This is your new *vault*.",
-      ); // Example assertion
+      expect(res.value["result-filepath"]).toBe("note/get/note-1.md");
+      expect(res.value["result-body"]).not.toEqual("");
+      expect(res.value["result-content"]).toContain("# Hello");
+      expect(res.value["result-front-matter"])
+        .toBe("tags:\n  - one\n  - two\nid: 01JV9K2XGJA4HH5XVWKC8EPQ4W\n");
+      expect(JSON.parse(res.value["result-properties"]))
+        .toEqual({ tags: ["one", "two"], id: "01JV9K2XGJA4HH5XVWKC8EPQ4W" });
+      expect(res.value["result-uri-path"])
+        .toContain("file=note%2Fget%2Fnote-1.md");
     }
   }, 10000); // Increase timeout for the test
 });
