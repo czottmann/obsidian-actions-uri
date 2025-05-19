105cbeef2d9fd90fa48fe022abaf866c7bfc9aa6
Carlo Zottmann <carlo@zottmann.dev>
2025-05-19 17:03:59 +0200

    [NEW] Adds `/note/open` tests related to periodic notes

--- tests/plugin-test-vault.original/note/open/noteOpen.test.ts
+++ tests/plugin-test-vault.original/note/open/noteOpen.test.ts
@@ -1,4 +1,7 @@
-import { callObsidian } from "#tests/helpers";
+import * as fs from "fs/promises";
+import * as path from "path";
+import { callObsidian, pause } from "#tests/helpers";
+import { periodicNotes, recentPeriodicNotes } from "#tests/periodic-notes";
 
 describe("note/open", () => {
   it("should open a note by its file name", async () => {
@@ -17,7 +20,49 @@ describe("note/open", () => {
     expect(res.ok).toBe(true);
   });
 
-  it.todo("should open a periodic note");
+  it("should open periodic notes", async () => {
+    for (const p of periodicNotes) {
+      const res = await callObsidian("note/open", { "periodic-note": p.key });
+      expect(res.ok).toBe(true);
+    }
+  });
+
+  it("should open recent periodic notes", async () => {
+    expect(globalThis.__TEST_VAULT_PATH__).toBeDefined();
+    const vaultPath = globalThis.__TEST_VAULT_PATH__!;
+
+    // Gather the list of current periodic notes (these are created during vault
+    // launch), so we can move them out of the way in order to test the lookup
+    // of recent periodic notes.
+    const renames = periodicNotes.map((p) => {
+      const oldName = path.join(vaultPath, `${p.dateString}.md`);
+      return [oldName, oldName + ".bak"];
+    });
+
+    // Rename the current periodic notes for this test.
+    for (const [oldName, newName] of renames) {
+      try {
+        await fs.rename(oldName, newName);
+      } catch (e) {}
+    }
+
+    // Give Obsidian a moment to index the changes
+    await pause(1000);
+
+    try {
+      for (const p of recentPeriodicNotes) {
+        const res = await callObsidian("note/open", { "periodic-note": p.key });
+        expect(res.ok).toBe(true);
+      }
+    } finally {
+      // Change the current periodic notes back to their original names.
+      for (const [oldName, newName] of renames) {
+        try {
+          await fs.rename(newName, oldName);
+        } catch (e) {}
+      }
+    }
+  });
 
   it("should return an error when the requested note doesn't exist", async () => {
     const res1 = await callObsidian("note/open", { uid: "unknown-id" });
