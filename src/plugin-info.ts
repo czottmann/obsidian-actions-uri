a9f64e243e353488698a7e92fd4c82868c7c68a6
Carlo Zottmann <carlo@zottmann.dev>
2025-08-05 11:59:58 +0200

    [REL] Release 1.8.2

--- src/plugin-info.ts
+++ src/plugin-info.ts
@@ -1,5 +1,5 @@
 /* File will be overwritten by bin/release.sh! */
 export const PLUGIN_INFO = {
-  "pluginVersion": "1.8.1",
-  "pluginReleasedAt": "2025-05-22T13:50:22+0200"
+  "pluginVersion": "1.8.2",
+  "pluginReleasedAt": "2025-08-05T11:59:54+0200"
 }
